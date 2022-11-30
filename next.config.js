fcd225d9311d20e8fcabbf9a7af836a454022ea0
lufdev <luisfer738@gmail.com>
2022-11-30 01:30:55 -0500

    prueba con estilos

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "www.futbolred.com",
+      "i.ytimg.com",
+      "img.a.transfermarkt.technology",
+      "docplayer.es",
+      "e7.pngegg.com",
+      "img.nauticexpo.es",
+      "parasenderismo.com",
+    ],
+  },
+  swcMinify: true,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F2F2F2",
+        secondary: "#ffff",
+        gradienpri: "#87BF4E",
+        gradiensecon: "#F2E307",
+        botton: "#D93636",
+        post: "#F8F7E6",
+      },
+      screens: {
+        mobileg: "450px",
+        // => @media (min-width: 450px) { ... }
+      },
+      rotate: {
+        55: "55deg",
+      },
+      animation: {
+        tilt: "tilt 10s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(5deg)",
+          },
+
+          "75%": {
+            transform: "rotate(-5deg)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
