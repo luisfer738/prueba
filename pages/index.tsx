3fe67af549818bac19d93d071c63482943446658
lufdev <luisfer738@gmail.com>
2022-11-30 01:42:12 -0500

    prueba con components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
 import type { NextPage } from 'next'
+import SideBar from "../components/SideBar";
 import Head from 'next/head'
 
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 bg-botton flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-      </main>
-    </div>
+    <main className="flex min-h-screen max-w-7xl mx-auto  mobileg:max-sm:px-[50px]">
+      <SideBar />
+    </main>
   );
 };
 
